72e51b443385376ec09fa20af53cedf433a0eb85
Daniel Jackoway <danjdel@gmail.com>
2010-11-12 14:06:40 -0600

    base functionality works

---
+++ test/tc_nothing.rb
@@ -0,0 +1,15 @@
+require 'test/unit'
+require 'test/unit/ui/console/testrunner'
+
+class TC_testNothing < Test::Unit::TestCase
+
+  def setup
+  end
+
+  def teardown
+  end
+
+  def test_the_truth
+    assert true
+  end
+end
